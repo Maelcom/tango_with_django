97bdad3a4885b1f133e35766a51abb3ad70e5a73
Maelcom <Maelcom@users.noreply.github.com>
2014-10-04 21:00:28 +0400

    added views

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Rango says hello world!')
+    return HttpResponse('Rango says hello world!'
+                        'View <a href="about">"About"</a> page.')
+
+def about(request):
+    return HttpResponse('Rango Says: Here is the about page.'
+                        'Go back to <a href="/rango">"Rango"</a> page.')
