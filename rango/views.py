517de987a710e2a12fea6df39402ba82905ab03a
Maelcom <Maelcom@users.noreply.github.com>
2014-10-06 18:00:04 +0400

    added static and media

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,14 @@
-from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
 
 def index(request):
-    return HttpResponse('Rango says hello world!'
-                        'View <a href="about">"About"</a> page.')
+    context = RequestContext(request)
+    context_dict = {'boldmessage': "I am bold font from the context"}
+    return render_to_response('rango/index.html', context_dict, context)
+
 
 def about(request):
-    return HttpResponse('Rango Says: Here is the about page.'
-                        'Go back to <a href="/rango">"Rango"</a> page.')
+    context = RequestContext(request)
+    context_dict = {'file_name': "Vdul.jpg"}
+    return render_to_response('rango/about.html', context_dict, context)
