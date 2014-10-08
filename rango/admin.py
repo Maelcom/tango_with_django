66f86a0d1f1436e225d7c00143ecab97a9a42620
Maelcom <Maelcom@users.noreply.github.com>
2014-10-08 23:36:13 +0400

    autregister models via exquisite govnocode, see admin.py

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.db.models import get_models, get_app
 
-# Register your models here.
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+
+for model in get_models(get_app('rango')):
+    try:
+        admin.site.register(model, globals()[model.__name__+'Admin'])
+    except admin.sites.AlreadyRegistered:
+        pass
+    except (NameError, KeyError):
+        try:
+            admin.site.register(model)
+        except admin.sites.AlreadyRegistered:
+            pass
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -3,10 +3,6 @@ from django.contrib import admin
 from django.conf import settings
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tango_with_django_project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')),
 )
