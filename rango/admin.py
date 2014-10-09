9516a6ba7786b9a8fcc20640370bd4843ec500d9
Maelcom <Maelcom@users.noreply.github.com>
2014-10-09 17:56:58 +0400

    much better govnocode

--- rango/admin.py
+++ rango/admin.py
@@ -5,14 +5,8 @@ from django.db.models import get_models, get_app
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
-
 for model in get_models(get_app('rango')):
     try:
-        admin.site.register(model, globals()[model.__name__+'Admin'])
+        admin.site.register(model, locals().copy().get(model.__name__+'Admin'))
     except admin.sites.AlreadyRegistered:
         pass
-    except (NameError, KeyError):
-        try:
-            admin.site.register(model)
-        except admin.sites.AlreadyRegistered:
-            pass
