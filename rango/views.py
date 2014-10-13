be493ad6557361e40fdaedf080742099a3cb4c10
Maelcom <Maelcom@users.noreply.github.com>
2014-10-13 23:34:04 +0400

    +ModelForms

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
 
 
 def index(request):
@@ -24,7 +25,7 @@ def about(request):
 
 def category_view(request, category_name_url):
     context = RequestContext(request)
-    category_name = decode_ulr(category_name_url)
+    category_name = decode_url(category_name_url)
     context_dict = {'category_name': category_name}
 
     try:
@@ -38,9 +39,56 @@ def category_view(request, category_name_url):
     return render_to_response('rango/category.html', context_dict, context)
 
 
+def add_category(request):
+    context = RequestContext(request)
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return index(request)
+        else:
+            print form.errors
+    else:
+        form = CategoryForm()
+
+    return render_to_response('rango/add_category.html', {'form': form}, context)
+
+
+def add_page(request, category_name_url):
+    context = RequestContext(request)
+    category_name = decode_url(category_name_url)
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+
+        if form.is_valid():
+            page = form.save(commit=False)
+            page.views = 0
+            try:
+                page.category = Category.objects.get(name=category_name)
+            except Category.DoesNotExist:
+                return render_to_response('rango/add_category.html', {}, context)
+
+            page.save()
+
+            return category_view(request, category_name_url)
+        else:
+            print form.errors
+    else:
+        form = PageForm()
+
+    return render_to_response('rango/add_page.html',
+                              {'category_name_url': category_name_url,
+                               'category_name': category_name,
+                               'form': form},
+                              context)
+
+
 def encode_url(name):
     return name.replace(' ', '_')
 
 
-def decode_ulr(url):
+def decode_url(url):
     return url.replace('_', ' ')
